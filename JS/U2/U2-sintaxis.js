d20e37fd7109f9e202ede18c7a3cfbb8574ad852
Nahuelj <113739032+Nahuelj@users.noreply.github.com>
2022-10-10 21:15:24 -0300

    Add files via upload

---
+++ JS/U2/U2-sintaxis.js
@@ -0,0 +1,9 @@
+
+const libroFavorito = {
+    titulo: "Manual de vida",
+    autor: "Epicteto",
+    fecha: "23/04/2001",
+    url:"www.manualdevida.com",
+}
+
+let lista =["Nahuel", 21, false, new Date(2001, 3, 16), libroFavorito]
