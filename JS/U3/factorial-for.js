ff44d38c60363287a644548cc62fc96fbc088bac
Nahuelj <113739032+Nahuelj@users.noreply.github.com>
2022-10-12 14:27:31 -0300

    Add files via upload

---
+++ JS/U3/factorial-for.js
@@ -0,0 +1,15 @@
+// FACTORIAL
+
+
+let num = 10;
+let resultado = 1;
+
+
+for(i = num; i >= 1; i--){
+    resultado = resultado*i;
+}
+
+let respuesta_total = "El resultado del factorial de "+num+" es "+resultado;
+
+console.log(respuesta_total)
+
---
+++ JS/U3/factorial-while.js
@@ -0,0 +1,14 @@
+//FACTORIAL WHILE
+
+let num = 10;
+let resultado = 1;
+
+
+
+while(num >= 1 ){
+    resultado = resultado * num;
+    --num;
+}
+
+let respuesta = "El factorial de "+num+" es "+resultado
+console.log(respuesta)
